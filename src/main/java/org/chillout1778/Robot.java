b56702327640c21029d9fca874e2918b23e7c8aa
Simon Branch <simonmbranch@gmail.com>
2023-10-26 19:52:30 -0700

    Add MIT LICENSE and delete some silly WPILib comments

--- src/main/java/org/chillout1778/Robot.java
+++ src/main/java/org/chillout1778/Robot.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package org.chillout1778;
 
 import edu.wpi.first.wpilibj.Joystick;
@@ -9,10 +5,6 @@ import edu.wpi.first.wpilibj.PWMSparkMax;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-/**
- * This is a demo program showing the use of the RobotDrive class, specifically it contains the code
- * necessary to operate a robot with tank drive.
- */
 public class Robot extends TimedRobot {
   private DifferentialDrive m_myRobot;
   private Joystick m_leftStick;
