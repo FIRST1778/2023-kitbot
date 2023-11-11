13aa9af960dade79c95eeb53b3454f4579fdf8dc
Simon Branch <simonmbranch@gmail.com>
2023-11-11 13:00:23 -0800

    Add simple PID to MotorSpin subsystem

--- src/main/java/org/chillout1778/MotorSpin.java
+++ src/main/java/org/chillout1778/MotorSpin.java
@@ -3,21 +3,36 @@ package org.chillout1778;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.util.Units;
 
 public class MotorSpin extends SubsystemBase {
-    private CANSparkMax motor = new CANSparkMax(11, CANSparkMaxLowLevel.MotorType.kBrushless);
-    private boolean spinning = false;
+    private CANSparkMax motor = new CANSparkMax(11,
+            CANSparkMaxLowLevel.MotorType.kBrushless);
+
+    private static final double kP = 1.0;
+    private static final double kI = 0.0;
+    private static final double kD = 0.0;
+
+    private PIDController controller = new PIDController(kP, kI, kD);
+    private double desired = 0.0;  // rad/s
 
     void spin() {
-        if (spinning)
-            return;
-        // TODO: add code to start motor
-        spinning = true;
+        desired = 0.1;
     }
+
     void stop() {
-        if (!spinning)
-            return;
-        // TODO: add code to stop motor
-        spinning = false;
+        desired = 0.0;
+    }
+
+    private double angularVelocity() {
+        return Units.rotationsPerMinuteToRadiansPerSecond(
+                motor.getEncoder().getVelocity()
+        );
+    }
+
+    @Override
+    void periodic() {
+        double voltage = controller.calculate(angularVelocity(), desired);
     }
 }
