fbb0a1367df3e35a038b7216b52ef8afeacae469
Simon Branch <simonmbranch@gmail.com>
2023-12-27 21:18:00 -0800

    turns out we needed gyro after all

--- src/main/java/org/chillout1778/subsystems/Gyro.kt
+++ src/main/java/org/chillout1778/subsystems/Gyro.kt
@@ -1,8 +1,15 @@
 package org.chillout1778.subsystems
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import com.ctre.phoenix.sensors.Pigeon2
 
-//import com.ctre.phoenix.sensors.Pigeon2
-object Gyro : SubsystemBase() {
+object Gyro: SubsystemBase() {
+    private val pigeon = Pigeon2(21)
 
+    init {
+        /* pigeon.configMountPose(Pigeon2.AxisDirection.PositiveY,
+                Pigeon2.AxisDirection.PositiveZ, 500) */
+    }
+
+    val yaw get() = Math.toRadians(pigeon.yaw)
 }
