746795208bef104a64bea31627937abef4a1409f
Simon Branch <simonmbranch@gmail.com>
2024-01-15 15:08:34 -0800

    Prototyping hack

--- src/main/java/org/chillout1778/Robot.kt
+++ src/main/java/org/chillout1778/Robot.kt
@@ -5,6 +5,10 @@ import edu.wpi.first.wpilibj.RobotBase
 import edu.wpi.first.wpilibj.TimedRobot
 import edu.wpi.first.wpilibj2.command.CommandScheduler
 import org.chillout1778.subsystems.Swerve
+import com.revrobotics.CANSparkMax
+import com.revrobotics.CANSparkMaxLowLevel
+import edu.wpi.first.wpilibj.XboxController
+import edu.wpi.first.math.MathUtil
 
 object Robot : TimedRobot() {
     fun start() {
@@ -31,4 +35,20 @@ object Robot : TimedRobot() {
     override fun disabledInit() {
         //Swerve.disable()
     }
+
+    val leftMotor = CANSparkMax(15, CANSparkMaxLowLevel.MotorType.kBrushless)
+    val rightMotor = CANSparkMax(16, CANSparkMaxLowLevel.MotorType.kBrushless)
+
+    override fun teleopPeriodic() {
+        val v = MathUtil.applyDeadband(m_robotContainer!!.op.getRawAxis(3), 0.1)
+        leftMotor.set(v)
+        rightMotor.set(v)
+    }
+
+    override fun testInit() {
+        // Cancels all running commands at the start of test mode.
+        CommandScheduler.getInstance().cancelAll()
+    }
+
+    override fun testPeriodic() {}
 }
