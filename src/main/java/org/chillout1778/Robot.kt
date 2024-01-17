a7ead83d2789231c59ea84f253898d975e9e3048
beansnaeb <higgiodi000@edmonds15.org>
2024-01-16 18:57:32 -0800

    add shooter subsystem

--- src/main/java/org/chillout1778/Robot.kt
+++ src/main/java/org/chillout1778/Robot.kt
@@ -4,11 +4,9 @@ import edu.wpi.first.wpilibj.RobotBase
 import edu.wpi.first.wpilibj.TimedRobot
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.CommandScheduler
+import com.pathplanner.lib.path.PathPlannerPath
+import com.pathplanner.lib.auto.AutoBuilder
 
-import com.revrobotics.CANSparkMax
-import com.revrobotics.CANSparkMaxLowLevel
-import edu.wpi.first.wpilibj.XboxController
-import edu.wpi.first.math.MathUtil
 
 object Robot : TimedRobot() {
     fun start(){
@@ -18,17 +16,12 @@ object Robot : TimedRobot() {
 
     private var m_autonomousCommand: Command? = null
 
-    private var m_robotContainer: RobotContainer? = null
-
     /**
      * This function is run when the robot is first started up and should be used for any
      * initialization code.
      */
     override fun robotInit() {
         ctreConfigs = CTREConfigs
-        // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-        // autonomous chooser on the dashboard.
-        m_robotContainer = RobotContainer()
     }
 
     /**
@@ -70,14 +63,7 @@ object Robot : TimedRobot() {
         }
     }
 
-    val leftMotor = CANSparkMax(15, CANSparkMaxLowLevel.MotorType.kBrushless)
-    val rightMotor = CANSparkMax(16, CANSparkMaxLowLevel.MotorType.kBrushless)
-
-    override fun teleopPeriodic() {
-        val v = MathUtil.applyDeadband(m_robotContainer!!.op.getRawAxis(3), 0.1)
-        leftMotor.set(v)
-        rightMotor.set(v)
-    }
+    
 
     override fun testInit() {
         // Cancels all running commands at the start of test mode.
